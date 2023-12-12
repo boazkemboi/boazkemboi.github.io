079e29aa17a7d5070aa2d25d8ad9ef6bf6e13553
boazkemboi <152148556+boazkemboi@users.noreply.github.com>
2023-12-12 14:42:34 +0300

    Create process.php

---
+++ process.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $subject = test_input($_POST["subject"]);
+    $message = test_input($_POST["message"]);
+
+    $to = 'your-email@example.com'; // Replace with your email address
+    $headers = "From: $email\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo json_encode(["success" => true]);
+    } else {
+        echo json_encode(["success" => false]);
+    }
+} else {
+    echo json_encode(["success" => false, "error" => "Invalid request"]);
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
